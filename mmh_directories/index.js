4156e4ebc4b2c250675cccd9951c9bf2e3d42068
gurjeet <gv9810@gmail.com>
2017-11-13 03:14:32 +0000

    new files

---
+++ mmh_directories/index.js
@@ -0,0 +1,25 @@
+let Hapi = require('hapi');
+let Inert = require('inert');
+let Path = require('path');
+
+let server = new Hapi.Server();
+ server.connection({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+});
+
+server.register(Inert, (err) => {
+    if (err) throw err;
+});
+
+server.route({
+    method: 'GET',
+    path: '/foo/bar/baz/{param}',
+    handler: {
+        directory: { path: Path.join(__dirname, '/public') }
+    }
+});
+
+server.start((err) => {
+    if (err) throw err;
+});
